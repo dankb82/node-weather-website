0c6c18811f206c91e4341e6d7fc3b5dca3cd7640
Daniel B <danielbuompane@gmail.com>
2020-08-27 21:43:29 -0400

    Add humidity data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,21 +1,23 @@
-const request = require('postman-request');
-const { WEATHER_API_KEY } = require('../../constants/constants');
+const request = require("postman-request");
+const { WEATHER_API_KEY } = require("../../constants/constants");
 
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=${WEATHER_API_KEY}&query=${latitude},${longitude}&units=f`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback('Unable to connect to the weather service', undefined);
+      callback("Unable to connect to the weather service", undefined);
     } else if (body.error) {
-      callback('Cannot find location.', undefined);
+      callback("Cannot find location.", undefined);
     } else {
       const currentTemp = body.current.temperature;
       const feelsLike = body.current.feelslike;
       const weatherDescription = body.current.weather_descriptions;
+      const humidity = body.current.humidity;
       callback(
         undefined,
-        `${weatherDescription}: It is currently ${currentTemp} degrees and it feels like ${feelsLike} degrees`
+        `${weatherDescription}: It is currently ${currentTemp} degrees and it feels like ${feelsLike} degrees. 
+        The humidity is ${humidity}%`
       );
     }
   });
