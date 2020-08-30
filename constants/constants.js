dec5ecc33b26e39375439510a5e588e4429491f1
Daniel B <danielbuompane@gmail.com>
2020-08-30 18:40:29 -0400

    Bring in dotenv for api keys

--- constants/constants.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    GEO_LOCATOR_API_KEY:
-        'pk.eyJ1IjoiZGFua2I4MiIsImEiOiJja2RtNGltcWoxNTFrMnNwZGl2OXhqZ21mIn0.1LmuTSR84YCzVG3suRKPfg',
-    WEATHER_API_KEY: '569ecd164ab03c1798ec2d80ca1bb5eb',
-};
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,13 @@
-const request = require("postman-request");
-const { WEATHER_API_KEY } = require("../../constants/constants");
+const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=${WEATHER_API_KEY}&query=${latitude},${longitude}&units=f`;
+  const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=${latitude},${longitude}&units=f`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback("Unable to connect to the weather service", undefined);
+      callback('Unable to connect to the weather service', undefined);
     } else if (body.error) {
-      callback("Cannot find location.", undefined);
+      callback('Cannot find location.', undefined);
     } else {
       const currentTemp = body.current.temperature;
       const feelsLike = body.current.feelslike;
